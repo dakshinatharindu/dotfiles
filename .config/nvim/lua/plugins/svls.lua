6a092a679473c17501ea1df55b4693cb2b30b9a9
dakshinatharindu <tharinduokd@gmail.com>
2024-06-16 20:49:40 +0530

    add configurations for svls lsp

--- .config/nvim/lua/plugins/svls.lua
+++ .config/nvim/lua/plugins/svls.lua
@@ -3,4 +3,20 @@ return {
     "nvim-treesitter/nvim-treesitter",
     opts = { ensure_installed = { "verilog" } },
   },
+
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        svls = {
+          root_dir = function(fname)
+            return require("lspconfig.util").root_pattern(".git")(fname)
+              or require("lspconfig.util").find_git_ancestor(fname)
+          end,
+          cmd = { "svls" },
+          filetypes = { "verilog", "systemverilog" },
+        },
+      },
+    },
+  },
 }
