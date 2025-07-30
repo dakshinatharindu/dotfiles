8dd2720510fa73ea12ec8a38d8d422c412674a22
dakshinatharindu <dakshinatharindu99@gmail.com>
2025-07-30 16:51:05 -0400

    add lazygit installation

--- setup.sh
+++ setup.sh
@@ -2,6 +2,7 @@
 
 # Setup script for my development environment
 
+############## ZSH Configuration ###############
 echo "Installing ZSH and Oh My Zsh..."
 # Install ZSH
 sudo apt update && sudo apt install -y zsh curl git
@@ -16,3 +17,10 @@ ZSH_THEME="daveverwer"
 sed -i "s/^ZSH_THEME=.*/ZSH_THEME=\"$ZSH_THEME\"/" ~/.zshrc
 # Update plugins in .zshrc
 sed -i 's/^plugins=(git)$/plugins=(git zsh-autosuggestions zsh-syntax-highlighting)/' ~/.zshrc
+
+############# LAZYGIT ##########################
+echo "Installing LazyGit..."
+LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | \grep -Po '"tag_name": *"v\K[^"]*')
+curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/download/v${LAZYGIT_VERSION}/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
+tar xf lazygit.tar.gz lazygit
+sudo install lazygit -D -t /usr/local/bin/
