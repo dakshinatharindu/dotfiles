34c05609a8e3f2c075fca76142c63a44eb4ffb1d
dakshinatharindu <dakshinatharindu99@gmail.com>
2025-08-12 12:13:55 -0400

    change colorsheme to grubox material

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,13 +1,24 @@
 return {
+  -- add gruvbox
   {
-    "folke/tokyonight.nvim",
-    lazy = true,
-    opts = { style = "night" },
+    "sainnhe/gruvbox-material",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      -- Set the background to hard
+      vim.g.gruvbox_material_background = "hard"
+      -- Optional: Enable better performance
+      vim.g.gruvbox_material_better_performance = 1
+      -- Optional: Disable italic comments if you prefer
+      -- vim.g.gruvbox_material_disable_italic_comment = 1
+    end,
   },
+
+  -- Configure LazyVim to load gruvbox
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "tokyonight-night", -- or "tokyonight-storm", "tokyonight-moon", "tokyonight-day"
+      colorscheme = "gruvbox-material",
     },
   },
 }
